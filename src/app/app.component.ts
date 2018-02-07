57a6580e7a2f8bc0805044cd667035ff305b8af7
gauravprwl14 <gauravprwl14@gmail.com>
2018-02-08 02:38:22 +0530

    integrated UI for the graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
-import * as _ from 'lodash';
+import { Component, OnInit } from "@angular/core";
+import * as _ from "lodash";
+import GraphMasterData from "../utils/masterDataForTheGraph";
+import GraphNodeModel from "../model/graphNode.model";
+import GraphModel from "../model/graph.model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
-  abc = _.filter([1,2,3], (a) => a != 2);
+  sourceNode = null;
+  isSourceNodeSelected = false;
+  targetNode = null;
+  dist = 0;
+
+  graph = null;
+  nodesArr = null;
+
+  ngOnInit() {
+    this.createGraph();
+  }
+
+  createGraph() {
+    const graphNodesArr = [];
+    this.graph = new GraphModel(GraphMasterData);
+    console.log('%c this.graph ', 'background: lime; color: black', this.graph);
+    console.log(
+      '%c typeof  ',
+      'background: lime; color: black',
+      typeof this.graph.nodes
+    );
+    this.nodesArr = Object.keys(this.graph.nodes).map(
+      nodeKeys => this.graph.nodes[nodeKeys]
+    );
+    console.log(
+      '%c nodesArr ',
+      'background: lime; color: black',
+      this.nodesArr
+    );
+  }
+
+  onNodeContainerClick(e) {
+    const targetNodeObj = e.target.value;
+    const id = targetNodeObj.id;
+    const weight = targetNodeObj.weight;
+    const nType = targetNodeObj.nType;
+    const neighbors = targetNodeObj.neighbors;
+    if (!this.isSourceNodeSelected) {
+      this.sourceNode = new GraphNodeModel(id, weight, nType, neighbors);
+      this.isSourceNodeSelected = true;
+    } else {
+      this.targetNode = new GraphNodeModel(id, weight, nType, neighbors);
+    }
+  }
 }
